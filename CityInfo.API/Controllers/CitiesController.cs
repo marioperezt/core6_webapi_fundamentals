3ed17e73ced327ab2675101881dc41ea6fa67e9f
Mario Perez <marioperezt@gmail.com>
2022-09-05 00:35:25 -0600

    Repo Reloaded

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public CitiesController(CitiesDataStore citiesDataStore)
+        {
+            this._citiesDataStore = citiesDataStore;
+        }
+        [HttpGet]        
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            /*var temp = new JsonResult(CitiesDataStore.Current.Cities);
+            temp.StatusCode = 200;*/
+            return Ok(_citiesDataStore.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            //find city
+            var result = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+            //return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
